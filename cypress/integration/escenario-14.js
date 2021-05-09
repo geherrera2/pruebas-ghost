61afc6ee3f584e7d4257ea45770441b4974563d4
German Herrera <germaneherrera@gmail.com>
2021-05-09 12:29:30 -0500

    ajusta escenario 6

--- cypress/integration/escenario-14.js
+++ cypress/integration/escenario-14.js
@@ -1,7 +1,6 @@
 /// <reference types="cypress" />
 import {LoginPage} from '../page-objects/login-page';
 import faker from 'faker';
-import { PagePage } from '../page-objects/page-page';
 import { TagPage } from '../page-objects/tag-page';
 
 context('escenario-14-eliminar tag', () => {
--- cypress/integration/escenario-6.js
+++ cypress/integration/escenario-6.js
@@ -1,10 +1,24 @@
 /// <reference types="cypress" />
 import {LoginPage} from '../page-objects/login-page';
 import {PostPage} from '../page-objects/posts-page';
+import { TagPage } from '../page-objects/tag-page';
 import faker from 'faker';
 
 context('escenario-2', () => {
-    let listaPost = [];
+    let valueNameTag = '';
+
+    it('Crear Tag', () => {
+        valueNameTag = faker.lorem.word();
+        const loginPage = new LoginPage();
+        const tagPage = new TagPage();
+
+        loginPage.visitPage();
+        loginPage.login();
+        loginPage.naviateToPage('Tags');
+        tagPage.clickNewTag();
+        tagPage.insertName(valueNameTag);
+    
+    })
 
     it('Asociar tag a un post', () => {
 
@@ -16,7 +30,7 @@ context('escenario-2', () => {
         postPage.navigateToPostsPage();
         postPage.clickFirstElementPost();
         postPage.openSettings();
-        postPage.addTag('prueba tag');
+        postPage.addTag(valueNameTag);
         postPage.clickUpdatePost();
         postPage.navigateToPostsPage();
         loginPage.logOut(); 
