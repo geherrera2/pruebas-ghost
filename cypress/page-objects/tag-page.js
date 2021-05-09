a330297e1d6fb3e3dad622be846ccc2b4ad6b172
Manuel Ortiz Villamarin <m.ortizv2@uniandes.edu.co>
2021-05-09 12:22:48 -0500

    Escenario 12

--- cypress/page-objects/tag-page.js
+++ cypress/page-objects/tag-page.js
@@ -1,49 +1,65 @@
-export class TagPage{
+export class TagPage {
     clickNewTag() {
         cy.contains('New tag').first().click();
     }
 
-    insertName(value){
-        
+    insertName(value) {
+
         // cy.get('#tag-name').type(value);
-        cy.get('input[name=name]').click({force: true}).type(value);
-        cy.get('.gh-btn-blue').click({force: true});
+        cy.get('input[name=name]').click({ force: true }).type(value);
+        cy.get('.gh-btn-blue').click({ force: true });
         // cy.get('input[name=name]').type(`${value}{enter}`)
     }
 
-    clickFirstElementPost(){
+    clickFirstElementPost() {
         cy.get('ol.tags-list').children('.gh-tags-list-item').each(($el, index, $list) => {
-            if(index === 0){
+            if (index === 0) {
                 let idElemento = $el.attr('id');
-                cy.get(`#${idElemento} a[title*="Edit this post"]`).first().click({force: true})
+                cy.get(`#${idElemento} a[title*="Edit this post"]`).first().click({ force: true })
             }
         })
     }
 
-    selectTag(value){
+    selectTag(value) {
         cy.get('ol.tags-list').children('.gh-tags-list-item').each(($el, index, $list) => {
-            let texto = $el.children('.gh-tag-list-title').text().trim() ;
-            if(texto === value){
+            let texto = $el.children('.gh-tag-list-title').text().trim();
+            if (texto === value) {
                 let idElemento = $el.attr('id');
-                cy.get(`#${idElemento} .gh-tag-list-title`).first().click({force: true})
+                cy.get(`#${idElemento} .gh-tag-list-title`).first().click({ force: true })
             }
         })
     }
 
-    validateTag(value){
+    validateTag(value) {
         cy.get('ol.tags-list .gh-tags-list-item').should(($lis) => {
             console.log($lis);
-            if($lis.length > 0){
+            if ($lis.length > 0) {
                 expect($lis).to.not.contain(value);
-            }else{
+            } else {
                 expect($lis.length).to.equal(0);
             }
         });
     }
-    
-    deleteTag(){
+
+    deleteTag() {
         cy.get('.gh-btn-red').contains('Delete tag').click();
         cy.wait(500);
         cy.get('.modal-content  .modal-footer .gh-btn-red').contains('Delete').click();
     }
+
+    navigateToTagsPage() {
+        cy.get('a').contains(`${tagMenuText}`).click();
+        cy.wait(500);
+    }
+
+    assertTagCreated(tagName) {
+        cy.get('h3').parent('a').should('contain', tagName)
+        //expect(tagName).to.be.oneOf(cy.get('ol.tags-list').children('.gh-tags-list-item').children('a').children('h3').value);
+    }
+
+    navigateToTagsList() {
+        cy.visit('http://localhost:2368/ghost/#/tags');
+    }
 }
+
+export const tagMenuText = 'Tag';
