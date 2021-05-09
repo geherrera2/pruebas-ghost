60dd21ce56a2a75bb50e786441cbe61e5558a929
Luis Leon <l.leon@uniandes.edu.co>
2021-05-09 11:26:50 -0500

    Devolver cambio del archivo

--- cypress/env.js
+++ cypress/env.js
@@ -3,12 +3,3 @@
 export const environment = {
     'baseURL': 'http://localhost:2368/ghost/'
 };
-
-export const userLoginData = {
-    'username': 'l.leonb@uniandes.edu.co',
-    'password': '1010204202Luis'
-    //'username': 'carlosriverosv@gmail.com',
-    //'password': 'q1w2e3r4t5'
-    //'username': 'ge.herrera2@uniandes.edu.co',
-    //'password': 'bogota2020.'
-};
