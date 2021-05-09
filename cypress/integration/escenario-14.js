a7ae76a073c6d521f497f414cf085c2ad961f30a
Carlos Riveros <carlosriverosv@gmail.com>
2021-05-09 11:48:36 -0500

    fix function name and refactor in other scenarios

--- cypress/integration/escenario-14.js
+++ cypress/integration/escenario-14.js
@@ -14,7 +14,7 @@ context('escenario-14-eliminar tag', () => {
 
         loginPage.visitPage();
         loginPage.login();
-        loginPage.naviateToPage('Tags');
+        loginPage.navigateToPage('Tags');
         tagPage.clickNewTag();
         expect('Tags').to.equal('Tags');
         tagPage.insertName(valueNameTag);
@@ -28,7 +28,7 @@ context('escenario-14-eliminar tag', () => {
 
         loginPage.visitPage();
         loginPage.login();
-        loginPage.naviateToPage('Tags');
+        loginPage.navigateToPage('Tags');
         tagPage.selectTag(valueNameTag);
         tagPage.deleteTag();
         tagPage.validateTag(valueNameTag);
