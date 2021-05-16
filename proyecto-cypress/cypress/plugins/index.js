6d017420594c2067b9ea2983d6bc4beb198bf132
German Herrera <germaneherrera@gmail.com>
2021-05-16 12:15:32 -0500

    Implementa proyeto backstop

--- proyecto-cypress/cypress/plugins/index.js
+++ proyecto-cypress/cypress/plugins/index.js
@@ -1,22 +1,52 @@
 /// <reference types="cypress" />
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
 
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
+const backstop = require('backstopjs');
+const config = require('../fixtures/backstop-base.json')
 
+
+/**
+ * Run BackstopJS as code 
+ * Scenarios and Report folder name can be set via params
+*/
+function backstopExec(payload = {scenarios: [], folder:'default'}, refreshReferences = true) {
+  return new Promise((resolve) => {
+    const configuration = config
+    config.scenarios = payload.scenarios
+    config.paths.html_report = `cypress/reports/backstop/${payload.folder}/html_report`
+    config.paths.ci_report = `cypress/reports/backstop/${payload.folder}/ci_report`
+
+    if (refreshReferences) {
+      backstop('reference', { config: configuration }).then(() => {
+        resolve(null)
+      }).catch(() => {
+        resolve(null)
+      })
+    } else {
+      backstop('test', { config: configuration }).then(() => {
+        resolve(null)
+      }).catch(() => {
+        resolve(null)
+      })
+    }
+
+  })
+}
 /**
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-}
+  backstop('init')
+   on('task', {
+    log(message) {
+      console.log(message)
+      return null
+    },
+    backstopRefreshReferences(customConfig) {
+      return backstopExec(customConfig)
+    },
+    backstopTest(customConfig) {
+      return backstopExec(customConfig, false)
+    }
+  })
+};
