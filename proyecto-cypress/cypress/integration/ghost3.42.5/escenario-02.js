348faf2e24b2f0eab0b5f511aed66274ea870e22
German Herrera <germaneherrera@gmail.com>
2021-05-15 21:18:03 -0500

    Ajusta escenarios ghost3.42.5

--- proyecto-cypress/cypress/integration/ghost3.42.5/escenario-02.js
+++ proyecto-cypress/cypress/integration/ghost3.42.5/escenario-02.js
@@ -2,6 +2,7 @@
 import {LoginPage} from '../../page-objects/login-page';
 import {PostPage} from '../../page-objects/posts-page';
 import faker from 'faker';
+import { GeneralPage } from '../../page-objects/general-page';
 
 context('escenario-2:Editar Post', () => {
 
@@ -9,13 +10,19 @@ context('escenario-2:Editar Post', () => {
     const postPage = new PostPage();
 
     it('Editar Post', () => {
+        Cypress.on('uncaught:exception', (err, runnable) => {
+            return false;
+          });
         const valueTitlePost = faker.lorem.sentence();
 
         loginPage.visitPage("3.42.5");
+        GeneralPage.stepScreenshot('1');
         loginPage.login();
         postPage.navigateToPostsPage();
+        GeneralPage.stepScreenshot('2',100);
         postPage.clickFirstElementPost();
         postPage.updateTitlePost(valueTitlePost);
+        GeneralPage.stepScreenshot('3',100);
         postPage.clickUpdatePost();
         postPage.navigateToPostsPage();
         postPage.assertUpdatePost(valueTitlePost);
