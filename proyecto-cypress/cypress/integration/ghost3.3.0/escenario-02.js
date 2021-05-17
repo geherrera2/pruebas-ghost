e444bed7d4b3742d76f343f180d02546bf6ed509
German Herrera <germaneherrera@gmail.com>
2021-05-16 21:24:49 -0500

    Ajusta escenario 2

--- proyecto-cypress/cypress/integration/ghost3.3.0/escenario-02.js
+++ proyecto-cypress/cypress/integration/ghost3.3.0/escenario-02.js
@@ -9,44 +9,22 @@ context('escenario-2:Editar Post', () => {
     const loginPage = new LoginPage();
     const postPage = new PostPage();
 
-    // it('Editar Post', () => {
-    //     const valueTitlePost = faker.lorem.sentence();
+    it('Editar Post', () => {
+        const valueTitlePost = faker.lorem.sentence();
 
-    //     loginPage.visitPage();
-    //     GeneralPage.stepScreenshot('1');
-    //     loginPage.login();
-    //     postPage.navigateToPostsPage();
-    //     GeneralPage.stepScreenshot('2',100);
-    //     postPage.clickFirstElementPost();
-    //     postPage.updateTitlePost(valueTitlePost);
-    //     GeneralPage.stepScreenshot('3',100);
-    //     postPage.clickUpdatePost();
-    //     postPage.navigateToPostsPage();
-    //     postPage.assertUpdatePost(valueTitlePost);
-    //     loginPage.logOut();
+        loginPage.visitPage();
+        GeneralPage.stepScreenshot('1');
+        loginPage.login();
+        postPage.navigateToPostsPage();
+        GeneralPage.stepScreenshot('2',100);
+        postPage.clickFirstElementPost();
+        postPage.updateTitlePost(valueTitlePost);
+        GeneralPage.stepScreenshot('3',100);
+        postPage.clickUpdatePost();
+        postPage.navigateToPostsPage();
+        postPage.assertUpdatePost(valueTitlePost);
+        loginPage.logOut();
         
     
-    // })
-
-    let pages = [
-        { label: 'base', url: `http://localhost:2368/ghost/`, hideSelectors },
-       
-      ]
-
-    it('Generate report using Backstop JS', function () {
-        // If ref URl is set, then it means we are in the right env for doing both refresh and test calls
-        let refScenarios = []
-        let testScenarios = []
-        pages.forEach((scenario) => {
-          const refScenarioUrl = scenario.url.replace(baseUrl, refUrl)
-          const refScenario = { ...scenario }
-          const testScenario = { ...scenario }
-          refScenario.url = refScenarioUrl
-          refScenarios.push(refScenario)
-          testScenarios.push(testScenario)
-        })
-        cy.task('backstopRefreshReferences', { scenarios: refScenarios, folder: 'paternlab' }, { timeout: 820000 })
-        cy.task('backstopTest', { scenarios: testScenarios, folder: 'patternlab' }, { timeout: 820000 })
-        
-      })
+    })
   })
