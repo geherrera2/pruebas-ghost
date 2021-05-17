41d74af64734b47de174507cbac2b71cf1d572cd
German Herrera <germaneherrera@gmail.com>
2021-05-16 22:13:50 -0500

    Ajusta nombres screenshot

--- proyecto-cypress/cypress/integration/ghost3.42.5/escenario-07.js
+++ proyecto-cypress/cypress/integration/ghost3.42.5/escenario-07.js
@@ -13,28 +13,28 @@ context('escenario-7', () => {
         const valueTitlePage = faker.lorem.words(5);
         const valueBodyPage = faker.lorem.word();
         loginPage.visitPage();
-        GeneralPage.stepScreenshot('01');
+        GeneralPage.stepScreenshot('1');
         loginPage.login();
-        GeneralPage.stepScreenshot('02');
+        GeneralPage.stepScreenshot('2');
         pagePage.navigateToPagesPage();
-        GeneralPage.stepScreenshot('03');
+        GeneralPage.stepScreenshot('3');
         cy.get('ol.gh-list').then(listing => {
             let count = Cypress.$('.gh-posts-list-item').length;
             pagePage.clickNewPage();
-            GeneralPage.stepScreenshot('04');
+            GeneralPage.stepScreenshot('4');
             pagePage.fillPageTitle(valueTitlePage);
             pagePage.returnList();
             pagePage.clickFirstElementPage
             pagePage.fillPageBody(valueBodyPage);
-            GeneralPage.stepScreenshot('05');
+            GeneralPage.stepScreenshot('5');
             pagePage.openPublish();
-            GeneralPage.stepScreenshot('06');
+            GeneralPage.stepScreenshot('6');
             pagePage.publish();
-            GeneralPage.stepScreenshot('07');
+            GeneralPage.stepScreenshot('7');
             pagePage.returnList();
             cy.wait(1000);
             cy.get('ol.gh-list').children('.gh-posts-list-item').should('have.length', count+1);
-            GeneralPage.stepScreenshot('08');
+            GeneralPage.stepScreenshot('8');
         })
     })
 
