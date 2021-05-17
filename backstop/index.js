92953ac08ae8bc8ebb5b57239b48b8ba98f19950
German Herrera <germaneherrera@gmail.com>
2021-05-16 21:25:07 -0500

    Test generar archivo backstop

---
+++ backstop/index.js
@@ -0,0 +1,154 @@
+var dataScenarios = [];
+var scenarios = [
+    {
+        "nombre": "escenario-01.js",
+        "step": 5,
+        "prefijo": "step0"
+    },
+    {
+        "nombre": "escenario-02.js",
+        "step": 3,
+        "prefijo": "step0"
+    },
+    {
+        "nombre": "escenario-03.js",
+        "step": 5,
+        "prefijo": "step00"
+    },
+    {
+        "nombre": "escenario-04.js",
+        "step": 11,
+        "prefijo": "step00"
+    },
+    {
+        "nombre": "escenario-05.js",
+        "step": 9,
+        "prefijo": "step0"
+    },
+    {
+        "nombre": "escenario-06.js",
+        "step": 6,
+        "prefijo": "step0"
+    },
+    {
+        "nombre": "escenario-07.js",
+        "step": 7,
+        "prefijo": "step00"
+    },
+    {
+        "nombre": "escenario-08.js",
+        "step": 11,
+        "prefijo": "step00"
+    },
+    {
+        "nombre": "escenario-09.js",
+        "step": 7,
+        "prefijo": "step0"
+    },
+    {
+        "nombre": "escenario-10.js",
+        "step": 7,
+        "prefijo": "step0"
+    },
+    {
+        "nombre": "escenario-11.js",
+        "step": 7,
+        "prefijo": "step00"
+    },
+    {
+        "nombre": "escenario-12.js",
+        "step": 5,
+        "prefijo": "step00"
+    },
+    {
+        "nombre": "escenario-13.js",
+        "step": 8,
+        "prefijo": "step0"
+    },
+    {
+        "nombre": "escenario-14.js",
+        "step": 6,
+        "prefijo": "step0"
+    },
+    {
+        "nombre": "escenario-15.js",
+        "step": 5,
+        "prefijo": "step00"
+    },
+    {
+        "nombre": "escenario-16.js",
+        "step": 6,
+        "prefijo": "step00"
+    },
+    {
+        "nombre": "escenario-17.js",
+        "step": 9,
+        "prefijo": "step0"
+    },
+    {
+        "nombre": "escenario-18.js",
+        "step": 4,
+        "prefijo": "step0"
+    },
+    {
+        "nombre": "escenario-19.js",
+        "step": 4,
+        "prefijo": "step00"
+    },
+    {
+        "nombre": "escenario-20.js",
+        "step": 7,
+        "prefijo": "step00"
+    }
+]
+
+
+
+scenarios.forEach( (e)=>{
+    for (let index = 1; index <= e.step; index++) {
+        dataScenarios.push({
+            "label": "step0"+index,
+            "url": "../proyecto-cypress/cypress/screenshots/ghost3.3.0/"+e.nombre+"/step0"+index+".png",
+            "referenceUrl": "../proyecto-cypress/cypress/screenshots/ghost3.3.0/"+e.nombre+"/step0"+index+".png",
+            "misMatchThreshold" : 0.1,
+          })
+    }
+});
+
+
+var dataBackstop = {
+    "id": "backstop_default",
+    "viewports": [
+      {
+        "label": "default",
+        "width": 800,
+        "height": 600
+      }
+    ],
+    "onBeforeScript": "puppet/onBefore.js",
+    "onReadyScript": "puppet/onReady.js",
+    "scenarios": dataScenarios,
+    "paths": {
+      "bitmaps_reference": "backstop_data/bitmaps_reference",
+      "bitmaps_test": "backstop_data/bitmaps_test",
+      "engine_scripts": "backstop_data/engine_scripts",
+      "html_report": "backstop_data/html_report",
+      "ci_report": "backstop_data/ci_report"
+    },
+    "report": ["browser"],
+    "engine": "puppeteer",
+    "engineOptions": {
+      "args": ["--no-sandbox"]
+    },
+    "asyncCaptureLimit": 5,
+    "asyncCompareLimit": 50,
+    "debug": false,
+    "debugWindow": false
+  };
+
+    var dictstring = JSON.stringify(dataBackstop);
+
+var fs = require('fs');
+fs.writeFile("backstop-test.json", dictstring, function(err, result) {
+    if(err) console.log('error', err);
+});
