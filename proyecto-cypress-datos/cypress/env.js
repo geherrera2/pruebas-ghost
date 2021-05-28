641a88c7c42527a04f65e026552aabc352c80cd2
German Herrera <germaneherrera@gmail.com>
2021-05-27 19:51:10 -0500

    Mover carpetas de proyecto

--- proyecto-cypress-datos/cypress/env.js
+++
@@ -1,6 +0,0 @@
-
-
-export const environment = {
-    'baseURL': 'http://localhost:2369/ghost/',
-    'baseURLNew': 'http://localhost:2369/ghost/',
-};
--- proyecto-cypress/cypress/env.js
+++
@@ -1,6 +0,0 @@
-
-
-export const environment = {
-    'baseURL': 'http://localhost:2368/ghost/',
-    'baseURLNew': 'http://localhost:2369/ghost/',
-};
